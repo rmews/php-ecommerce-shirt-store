2d1373275c79d823bc1f81c9ea6c4d7259fdd8fb
Robert Mews <robert.mews@gmail.com>
2015-04-22 20:38:57 -0400

    Added new functionality that pulls data from a shirts database

--- inc/config.php
+++ inc/config.php
@@ -5,3 +5,11 @@
 
 	define("BASE_URL","/treehouse_shirt_store/");
 	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/treehouse_shirt_store/");
+
+	// define database connect 
+
+	define("DB_HOST","DB SERVER NAME");
+	define("DB_NAME","DB SCHEMA NAME");
+	define("DB_PORT","PORT IF NEEDED"); // default: 3306
+	define("DB_USER","YOUR DB USER NAME");
+	define("DB_PASS","YOUR DB PASS");
